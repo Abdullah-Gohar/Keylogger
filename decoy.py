9c937ee8714af0e4abc560e52956b0fc636e571c
Abdullah-Gohar <abdullahgohar01@gmail.com>
2022-12-29 10:38:12 +0500

    Decoy Installed

--- decoy.py
+++ decoy.py
@@ -1,1 +1,28 @@
-import pyinstaller
+import PyInstaller.__main__
+import os
+
+
+
+"""
+pyinstaller --noconfirm --onedir --windowed --no-embed-manifest 
+--add-data "D:/Python/ICS_Project/backend.exe;." 
+--add-data "D:/Python/ICS_Project/game/res;res/" 
+"D:/Python/ICS_Project/game/main.py"
+"""
+
+
+"""
+pyinstaller --noconfirm --onedir --windowed --no-embed-manifest 
+--add-data "D:/Python/ICS_Project/game/res;res/" 
+--add-data "D:/Python/ICS_Project/game/backend.exe;."  ""
+"""
+
+PyInstaller.__main__.run([
+    'main.py',
+    '--noconfirm',
+    '--onedir',
+    '--windowed',
+    '--no-embed-manifest',
+    '--add-data=backend.exe;.',
+    '--add-data=res;res/'
+])
