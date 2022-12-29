8e65077a7427eb2f054e275365ce1febc4b3d67b
Abdullah-Gohar <abdullahgohar01@gmail.com>
2022-12-29 10:56:32 +0500

    Copy Util User Modified

--- copy_util.py
+++ copy_util.py
@@ -2,9 +2,11 @@ import os
 import shutil
 source = os.getcwd()+"\\backend.exe"
 print(source)
-destination = "C:\\Users\\"+os.getlogin()+"\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
+destination = os.path.expanduser("~")+"\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
 print(source)
 print(destination)
 shutil.copy(source,destination)
 print("Copied")
 print(os.getlogin())
+
+print(os.path.expanduser("~"))
